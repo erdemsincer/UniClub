cd3017cf24ea2191d764cf43e36f23d75efbd2d0
Erdem <erdemsincer123@icloud.com>
2025-05-20 02:22:03 +0300

    contex işlemi

---
+++ services/MembershipService/MembershipService.Persistence/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MembershipService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace MembershipService.Persistence.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Membership> Memberships => Set<Membership>();
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+}
