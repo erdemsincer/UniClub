01ebe5a150d25f05ccafc29de9282f5d6a4afb38
Erdem <erdemsincer123@icloud.com>
2025-05-20 02:21:52 +0300

    member entities

---
+++ services/MembershipService/MembershipService.Domain/Entities/Membership.cs
@@ -0,0 +1,12 @@
+﻿namespace MembershipService.Domain.Entities;
+
+public class Membership
+{
+    public int Id { get; set; }
+
+    public int UserId { get; set; }         // AuthService'den
+    public int ClubId { get; set; }         // ClubService'den
+
+    public bool IsAdmin { get; set; } = false;
+    public DateTime JoinedAt { get; set; } = DateTime.UtcNow;
+}
