4f9592013e61fdcfc283a64ca342ab2aa77cada4
Erdem <erdemsincer123@icloud.com>
2025-05-19 23:05:07 +0300

    clubservice domain katmanı hazırlandfı

---
+++ services/ClubService/ClubService.Domain/Entities/Club.cs
@@ -0,0 +1,11 @@
+﻿namespace ClubService.Domain.Entities;
+
+public class Club
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string LogoUrl { get; set; } = string.Empty;
+    public int CreatedByUserId { get; set; }  // JWT içinden gelen UserId
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
