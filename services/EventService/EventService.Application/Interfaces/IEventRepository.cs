c72b21c7affdc7e8a8657727a371cbecd92b5232
Erdem <erdemsincer123@icloud.com>
2025-05-20 02:41:56 +0300

    interfaceler

---
+++ services/EventService/EventService.Application/Interfaces/IEventRepository.cs
@@ -0,0 +1,14 @@
+﻿using EventService.Domain.Entities;
+
+namespace EventService.Application.Interfaces;
+
+public interface IEventRepository
+{
+    Task<List<Event>> GetAllAsync();
+    Task<List<Event>> GetByClubIdAsync(int clubId);
+    Task<Event?> GetByIdAsync(int id);
+    Task AddAsync(Event ev);
+    Task UpdateAsync(Event ev);
+    Task DeleteAsync(Event ev);
+    Task<bool> IsOwnerAsync(int eventId, int userId);
+}
---
+++ services/EventService/EventService.Application/Interfaces/IEventService.cs
@@ -0,0 +1,13 @@
+﻿using EventService.Application.Dtos;
+
+namespace EventService.Application.Interfaces;
+
+public interface IEventService
+{
+    Task<List<EventDto>> GetAllAsync();
+    Task<List<EventDto>> GetByClubIdAsync(int clubId);
+    Task<EventDto?> GetByIdAsync(int id);
+    Task<int> CreateAsync(CreateEventDto dto, int userId);
+    Task<bool> UpdateAsync(int id, UpdateEventDto dto, int userId);
+    Task<bool> DeleteAsync(int id, int userId);
+}
