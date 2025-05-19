cbd890ba0d344fdfb1e03f55835b213a7daa973c
Erdem <erdemsincer123@icloud.com>
2025-05-20 02:41:10 +0300

    entities

---
+++ services/EventService/EventService.Domain/Entities/Event.cs
@@ -0,0 +1,17 @@
+﻿namespace EventService.Domain.Entities;
+
+public class Event
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+
+    public DateTime StartTime { get; set; }
+    public DateTime EndTime { get; set; }
+
+    public int ClubId { get; set; }              // Hangi kulübe ait?
+    public int CreatedByUserId { get; set; }     // JWT'den alınır
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
