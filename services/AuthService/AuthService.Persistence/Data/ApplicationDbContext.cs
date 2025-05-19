27e77276bab8044c273b978f577d66526f81f250
Erdem <erdemsincer123@icloud.com>
2025-05-19 23:06:52 +0300

    AUTH SERVİCE PERSİSTANCE

---
+++ services/AuthService/AuthService.Persistence/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AuthService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.Persistence.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users => Set<User>();
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+    }
+
+}
---
+++ services/AuthService/AuthService.Persistence/Repositories/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using AuthService.Domain.Entities;
+using AuthService.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+
+public class UserRepository : IUserRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public UserRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User?> GetByEmailAsync(string email)
+    {
+        return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+    }
+
+    public async Task AddUserAsync(User user)
+    {
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+    }
+}
