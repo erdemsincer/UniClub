d36ab9caf6181bbe08101dc98a5042188d063119
Erdem <erdemsincer123@icloud.com>
2025-05-20 02:41:35 +0300

    contex sınıfı

---
+++ services/EventService/EventService.Persistence/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EventService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace EventService.Persistence.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public DbSet<Event> Events => Set<Event>();
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+}
