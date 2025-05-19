e2acc12bb4d3456cdb623a173526a4c8dec8af6c
Erdem <erdemsincer123@icloud.com>
2025-05-19 23:07:12 +0300

    AUTH SERVİCE INFASTRUCTURE

---
+++ services/AuthService/AuthService.Infrastructure/Services/AuthService.cs
@@ -0,0 +1,65 @@
+﻿using AuthService.Application.Dtos;
+using AuthService.Application.Interfaces;
+using AuthService.Domain.Entities;
+using AuthService.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthService.Infrastructure.Services
+{
+    public class AuthService:IAuthService
+    {
+        private readonly IUserRepository _userRepo;
+        private readonly IJwtTokenGenerator _jwt;
+
+        public AuthService(IUserRepository userRepo, IJwtTokenGenerator jwt)
+        {
+            _userRepo = userRepo;
+            _jwt = jwt;
+        }
+
+        public async Task<AuthResponseDto> RegisterAsync(RegisterDto dto)
+        {
+            var existingUser = await _userRepo.GetByEmailAsync(dto.Email);
+            if (existingUser != null) throw new Exception("Email already registered");
+
+            var user = new User
+            {
+                FullName = dto.FullName,
+                Email = dto.Email,
+                PasswordHash = BCrypt.Net.BCrypt.HashPassword(dto.Password),
+                Role = Role.Student
+            };
+
+            await _userRepo.AddUserAsync(user);
+
+            var token = _jwt.GenerateToken(user);
+
+            return new AuthResponseDto
+            {
+                Email = user.Email,
+                Role = user.Role.ToString(),
+                Token = token
+            };
+        }
+
+        public async Task<AuthResponseDto> LoginAsync(LoginDto dto)
+        {
+            var user = await _userRepo.GetByEmailAsync(dto.Email);
+            if (user == null || !BCrypt.Net.BCrypt.Verify(dto.Password, user.PasswordHash))
+                throw new Exception("Invalid credentials");
+
+            var token = _jwt.GenerateToken(user);
+
+            return new AuthResponseDto
+            {
+                Email = user.Email,
+                Role = user.Role.ToString(),
+                Token = token
+            };
+        }
+    }
+}
---
+++ services/AuthService/AuthService.Infrastructure/Services/JwtTokenGenerator.cs
@@ -0,0 +1,39 @@
+﻿using AuthService.Domain.Entities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+public class JwtTokenGenerator : IJwtTokenGenerator
+{
+    private readonly IConfiguration _configuration;
+
+    public JwtTokenGenerator(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public string GenerateToken(User user)
+    {
+        var jwtSettings = _configuration.GetSection("JwtSettings");
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Key"]!));
+
+        var claims = new[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email),
+            new Claim(ClaimTypes.Role, user.Role.ToString())
+        };
+
+        var token = new JwtSecurityToken(
+            issuer: jwtSettings["Issuer"],
+            audience: jwtSettings["Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(Convert.ToDouble(jwtSettings["ExpireInMinutes"])),
+            signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha256)
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
