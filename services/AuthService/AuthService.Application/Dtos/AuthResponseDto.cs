6e46582e133802631befb4b5ffb0af54f61a2a66
Erdem <erdemsincer123@icloud.com>
2025-05-19 23:06:43 +0300

    auth dtos

---
+++ services/AuthService/AuthService.Application/Dtos/AuthResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthService.Application.Dtos
+{
+    public class AuthResponseDto
+    {
+        public string Token { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+    }
+}
