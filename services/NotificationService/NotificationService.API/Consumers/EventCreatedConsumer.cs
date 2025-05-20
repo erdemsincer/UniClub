47c953a30f7fc3ad5509f6fcedbc60a34564769f
Erdem <erdemsincer123@icloud.com>
2025-05-20 16:40:41 +0300

    consumers

---
+++ services/NotificationService/NotificationService.API/Consumers/EventCreatedConsumer.cs
@@ -0,0 +1,14 @@
+﻿using MassTransit;
+using UniClub.Shared.Events;
+
+namespace NotificationService.API.Consumers;
+
+public class EventCreatedConsumer : IConsumer<EventCreatedEvent>
+{
+    public Task Consume(ConsumeContext<EventCreatedEvent> context)
+    {
+        var message = context.Message;
+        Console.WriteLine($"📨 Etkinlik Oluşturuldu: {message.Title} - {message.StartTime}");
+        return Task.CompletedTask;
+    }
+}
