029491ecec73ec35756d54c7d1695e848c70b33a
Erdem <erdemsincer123@icloud.com>
2025-05-20 17:34:15 +0300

    evenet

--- UniClub.Shared/Events/EventCreatedEvent.cs
+++ UniClub.Shared/Events/EventCreatedEvent.cs
@@ -5,5 +5,6 @@ public class EventCreatedEvent
     public int EventId { get; set; }
     public int ClubId { get; set; }
     public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
     public DateTime StartTime { get; set; }
 }
