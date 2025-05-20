b9e3135fc772741a798a3279084749709b5fbe77
Erdem <erdemsincer123@icloud.com>
2025-05-20 17:33:51 +0300

    mailservice

---
+++ services/NotificationService/NotificationService.Infrastructure/Services/MailService.cs
@@ -0,0 +1,43 @@
+﻿using NotificationService.Infrastructure.Settings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+
+namespace NotificationService.Infrastructure.Services
+{
+    public class MailService : IMailService
+    {
+        private readonly MailSettings _settings;
+
+        public MailService(IOptions<MailSettings> options)
+        {
+            _settings = options.Value;
+        }
+
+        public async Task SendEmailAsync(string to, string subject, string body)
+        {
+            var smtp = new SmtpClient(_settings.SmtpHost, _settings.SmtpPort)
+            {
+                Credentials = new NetworkCredential(_settings.Username, _settings.Password),
+                EnableSsl = true
+            };
+
+            var message = new MailMessage
+            {
+                From = new MailAddress(_settings.From),
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            };
+
+            message.To.Add(to);
+
+            await smtp.SendMailAsync(message);
+        }
+    }
+}
